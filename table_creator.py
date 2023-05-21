831bf0a0c71aaad0547e1f6ba8d660215547d757
Devon Ankar <devon.ankar@gmail.com>
2023-05-21 13:44:03 -0400

    Rename sample -> preview

--- table_creator.py
+++ table_creator.py
@@ -12,7 +12,7 @@ end_marker = '\n\n<!-- END HIGHLIGHTS -->'
 files_to_update = (
     'index.md',
     '404.md',
-    'sample.md',
+    'preview.md',
 )
 
 order_text = open('table_order.txt').read()
